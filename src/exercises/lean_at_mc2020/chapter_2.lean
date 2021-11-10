6681b040241b32f7a352285fa1ae75974650b039
andrewparr <10243811+andrewparr@users.noreply.github.com>
2021-11-10 18:17:03 +0000

    current WIP on Barber paradox problem.

--- src/exercises/lean_at_mc2020/chapter_2.lean
+++ src/exercises/lean_at_mc2020/chapter_2.lean
@@ -1,5 +1,9 @@
+import tactic
+-- the next two lines let us use the by_cases tactic without trouble
+noncomputable theory
+open_locale classical
 
--- 2.1.2. Proofs as functions¶
+-- 2.1.2. Proofs as functions
 
 -- Every time you successfully construct a proof of a theorem say
 theorem tautology (P : Prop) : P → P :=
@@ -97,7 +101,81 @@ begin
   cases hPnP with hP hnP,
   exfalso,
   apply hnP,
-  exact hP,  
+  exact hP,
 end
 
 -- 2.3. Quantifiers
+
+-- 2.3.1. Barber paradox
+
+/--------------------------------------------------------------------------
+
+``by_cases``
+
+  If ``P`` is a proposition, then ``by_cases P,`` creates two goals,
+    the first with a hypothesis ``hp: P`` and
+    second with a hypothesis ``hp: ¬ P``.
+
+Delete the ``sorry,`` below and replace them with a legitimate proof.
+
+--------------------------------------------------------------------------/
+
+-- men is type.
+-- x : men means x is a man in the town
+-- shaves x y is inhabited if x shaves y
+
+variables (men : Type) (barber : men)
+variable  (shaves : men → men → Prop)
+
+/--------------------------------------------------------------------------
+The claim is that in a certain town there is a (male) barber that shaves all
+the men who do not shave themselves. (Why is this a paradox?) Prove that this
+is a contradiction.
+
+I found this complicated - the outline of the proof is as follows:
+
+Use proof by contradiction, so first assume
+  shaves barber x ↔ ¬ shaves x x
+I.e. we are assuming both
+  p1: shaves barber x → ¬ shaves x x
+and
+  p2: ¬ shaves x x → shaves barber x
+
+assume shaves barber x
+then by p1, we also have ¬ shaves x x
+but barber is a man and this means ¬ shaves barber x
+which is a contradiction.
+This contradiction means the assumption above is invalid and so ¬ assume shaves barber x
+
+Now assuming
+¬ shaves barber x
+then by p2 we must have shaves barber x
+this is also a contraction.
+
+This second contraction, means the initial assumption was wrong and so
+this completes the proof.
+---------------------------------------------------------------------------/
+
+example : ¬ (∀ x : men, shaves barber x ↔ ¬ shaves x x) :=
+begin
+  -- we prove by contradiction, so assume it's true
+  by_contradiction,
+  -- x is a barber
+  have x := barber,
+  -- h is currently (x : men), shaves barber x ↔ ¬shaves x x
+  -- so lets specialize to just get
+  -- h : h: shaves barber x ↔ ¬shaves x x
+  specialize h x,
+  -- since h is a ↔ hypothesis, we do cases on it.
+  cases h with p1 p2,
+  by_cases shaves barber barber,  
+  { 
+    -- goal here is false, and have h : shaves barber barber
+    
+    sorry,  
+  },
+  {
+    -- goal here is false and have h : ¬ shaves barber barber
+    sorry,
+  }
+end
